cc98057959af4f41af3daf8f43f4fe6edbb85d9d
Himanshu <himanshu32301@gmail.com>
2020-08-26 11:29:38 +0530

    Add Script and Change the design of the calculator

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// function for displaying values
+let screen = document.getElementById('screen');
+let keys = document.querySelectorAll('button');
+
+let screenValue = '';
+for(item of keys){
+  item.addEventListener('click', (e) =>{
+
+    buttonText = e.target.innerText;
+    console.log('Button iS ', buttonText)
+    if(buttonText=='X')
+    {
+        buttonText = '*';
+        screenValue += buttonText;
+        screen.value = screenValue;
+    }
+    else if(buttonText=='Clear')
+    {
+        screenValue = '';
+        screen.value = screenValue;
+    }
+    else if(buttonText=='=')
+    {
+        screen.value = eval(screenValue); 
+    }
+    else
+    {
+        screenValue += buttonText;
+        screen.value = screenValue;
+    }
+
+  })
+}
+
